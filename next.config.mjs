1a2d650ad011a265428ac468a6deb93ad39a4fc6
KamoCodes <217004345@student.uj.ac.za>
2025-07-22 22:10:03 +0000

    jnjn

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-const repoName = 'Me'; // Change if your repo name is different
 
 const nextConfig = {
   output: 'export',
@@ -14,8 +12,8 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: { unoptimized: true },
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  basePath: '',
+  assetPrefix: '',
 };
 
 export default nextConfig;
